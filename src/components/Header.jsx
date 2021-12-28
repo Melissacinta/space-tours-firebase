c05e81cdcbdaa4959d7ee18900b4b8b39bbfbae7
TechieNg <melissacinta@gmail.com>
2021-12-28 03:09:54 +0100

    still on mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import siteLogo from '../assets/shared/logo.svg';
 import { useState } from "react";
 import bars from '../assets/shared/icon-hamburger.svg';
+import close from '../assets/shared/icon-close.svg';
 import NavBar from '../components/NavBar';
 // import MobileMenu from '../components/MobileMenu';
 
 const Header = () => {
   const [show, setShow] = useState();
+
   return (
     <>
       <div className="flow">
         <header className="design-system__header">
           <img src={siteLogo} alt="site logo" className="design-logo" />
-          <NavBar txt="uppercase text-light ls-2" className={`${show ? "mobile-menu" : "NavBar"} flow`} onClick={() => { console.log("after clicked"); }} />
-          <img src={bars} alt="mobile menu" className="bars" onClick={() => setShow(!show)} />
+          <NavBar txt="uppercase text-light ls-2" className={`${show ? "mobile-menu" : "NavBar"} flow`} />
+          <button className="bars" onClick={() => setShow(!show)} style={{ backgroundImage: `url(${show ? close : bars})` }}> <span className="sr-only">Menu</span>
+          </button>
         </header>
       </div>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = ({ txt, className }) => {
   return (
-    <div className={className}>
+    <div className={className} onClick={() => { console.log("after clicked"); }} >
       <nav className={`${txt} ff-sans-cond`}>
         <ul className="nav-links flex underline-indicators">
           <li>
